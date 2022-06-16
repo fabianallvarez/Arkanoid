40d569c803370b5811370c2997e32231c838c61c
fabianallvarez <fabianallvarez@gmail.com>
2022-06-16 23:07:03 +0200

    FEAT: Usamos alias para pygame

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,22 +1,23 @@
-import pygame
+import pygame as pg
+
 from arkanoid import ANCHO, ALTO
 
 
 class Arkanoid:
     def __init__(self):
         print("arranca el juego")
-        pygame.init()
-        pygame.display.set_mode((ANCHO, ALTO))
+        pg.init()
+        pg.display.set_mode((ANCHO, ALTO))
 
     def jugar (self):
         """Este es el bucle principal"""
         salir = False
         while not salir:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
                     salir = True
             self.display.fill((99, 99, 99))
-            pygame.display.flip()
+            pg.display.flip()
 
 
 
